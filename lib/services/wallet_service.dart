22c884fe8ce488abfd1333d4c4ec070440f870ea
keninqiu <jack.li@fa.biz>
2020-01-23 10:28:14 -0500

    fix

--- lib/services/wallet_service.dart
+++ lib/services/wallet_service.dart
@@ -799,6 +799,8 @@ class WalletService {
           satoshisPerBytes);
       txHex = res1['txHex'];
       errMsg = res1['errMsg'];
+      print('txHex=' + txHex);
+      print('errMsg=' + errMsg);
       if (txHex != null && txHex != '') {
         if (doSubmit) {
           var res = await _api.postFabTx(txHex);
