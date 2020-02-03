c4a5257db7ac3d6b2002d78e903d1ae23fec937b
barry-ruprai <bhinderjit_ruprai@zoho.com>
2020-02-03 17:52:23 -0500

    Added release or debug text when build in the dashboard

--- lib/environments/environment.dart
+++ lib/environments/environment.dart
@@ -1,6 +1,6 @@
 import 'package:bitcoin_flutter/src/models/networks.dart';
 
-const bool isProduction = false;
+const bool isProduction = true;
 const bool isLocal = false;
 Map devConfig = {
   "chains": {
