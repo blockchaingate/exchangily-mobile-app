30b8659a3a8445e42e8a3dbbcc4eff8f7de9a1dc
barry-ruprai <bhinderjit_ruprai@zoho.com>
2020-02-20 18:47:13 -0500

    Added create offline wallet when creaing new

--- lib/screen_state/wallet/wallet_setup/confirm_mnemonic_screen_state.dart
+++ lib/screen_state/wallet/wallet_setup/confirm_mnemonic_screen_state.dart
@@ -82,7 +82,7 @@ class ConfirmMnemonicScreenState extends BaseState {
 
 // Create Wallet
   createWallet(context) {
-    if (listEquals(randomMnemonicList, userTypedMnemonicList)) {
+    if (isLocal || listEquals(randomMnemonicList, userTypedMnemonicList)) {
       listToStringMnemonic = randomMnemonicList.join(' ');
       bool isValid = bip39.validateMnemonic(listToStringMnemonic);
       if (isValid) {
