30d12ad8d79a03132ba8523e4d420ae58cfe50f7
barry-ruprai <bhinderjit_ruprai@zoho.com>
2020-02-20 20:25:07 -0500

    Fix language setting bug where on app reopen it does not load with the set language earlier

--- lib/environments/environment.dart
+++ lib/environments/environment.dart
@@ -1,7 +1,7 @@
 import 'package:bitcoin_flutter/src/models/networks.dart';
 
-const bool isProduction = true;
-const bool isLocal = false;
+const bool isProduction = false;
+const bool isLocal = true;
 
 Map devConfig = {
   "chains": {
