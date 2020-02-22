a8abf57aaf59da4157f65f91bcdb220a6f711f5a
barry-ruprai <bhinderjit_ruprai@zoho.com>
2020-02-21 20:30:39 -0500

    Updated localizations

--- lib/l10n/messages_all.dart
+++ lib/l10n/messages_all.dart
@@ -17,11 +17,13 @@ import 'package:intl/src/intl_helpers.dart';
 
 import 'messages_en.dart' as messages_en;
 import 'messages_messages.dart' as messages_messages;
+import 'messages_zh.dart' as messages_zh;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
   'en': () => new Future.value(null),
   'messages': () => new Future.value(null),
+  'zh': () => new Future.value(null),
 };
 
 MessageLookupByLibrary _findExact(String localeName) {
@@ -30,6 +32,8 @@ MessageLookupByLibrary _findExact(String localeName) {
       return messages_en.messages;
     case 'messages':
       return messages_messages.messages;
+    case 'zh':
+      return messages_zh.messages;
     default:
       return null;
   }
