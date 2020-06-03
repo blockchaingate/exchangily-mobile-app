d0452d22d28f41c25647a1646774d5b3f037b405
jacksun0124 <jacksun0124@gmail.com>
2020-06-03 14:03:37 -0400

    changed mnemonic page and some small things

--- lib/shared/globals.dart
+++ lib/shared/globals.dart
@@ -13,6 +13,7 @@
 import 'package:flutter/material.dart';
 
 final Color primaryColor = Color.fromRGBO(135, 31, 255, 1);
+final Color btnColor = Color.fromRGBO(125, 21, 255, 1);
 final Color secondaryColor = Color.fromRGBO(31, 33, 51, 1);
 final Color walletCardColor = Color.fromRGBO(43, 43, 75, 1);
 final Color iconBackgroundColor = Color.fromRGBO(83, 74, 223, 1);
