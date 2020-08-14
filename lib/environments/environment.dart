7a8cf0492afaea1e1773072c30086a863f7ba8c9
ken <kenin.qiu@gmail.com>
2020-08-14 10:42:40 -0400

    raise gase price

--- lib/environments/environment.dart
+++ lib/environments/environment.dart
@@ -32,7 +32,7 @@ Map devConfig = {
       "hardfork": 'byzantium',
       "chainId": 3,
       "infura": "https://ropsten.infura.io/v3/6c5bdfe73ef54bbab0accf87a6b4b0ef",
-      "gasPrice": 90000000000,
+      "gasPrice": 130000000000,
       "gasPriceMax": 200000000000,
       "gasLimit": 200000
     },
@@ -159,7 +159,7 @@ Map productionConfig = {
       "hardfork": 'byzantium',
       "chainId": 1,
       "infura": "https://mainnet.infura.io/v3/6c5bdfe73ef54bbab0accf87a6b4b0ef",
-      "gasPrice": 90000000000,
+      "gasPrice": 130000000000,
       "gasPriceMax": 200000000000,
       "gasLimit": 200000
     },
