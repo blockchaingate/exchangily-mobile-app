a9f4c18cdc1591825dd9b336502755d23b69c1c6
ken <kenin.qiu@gmail.com>
2020-09-09 14:40:38 -0400

    add flash pay function for mobile

--- lib/screen_state/settings/settings_screen_state.dart
+++ lib/screen_state/settings/settings_screen_state.dart
@@ -67,6 +67,7 @@ class SettingsScreenState extends BaseState {
   // Delete wallet and local storage
 
   Future deleteWallet() async {
+
     errorMessage = '';
     isDeleting = true;
     setBusy(true);
@@ -128,6 +129,16 @@ class SettingsScreenState extends BaseState {
         if (res.confirmed) {
           isVisible = !isVisible;
           mnemonic = res.returnedText;
+
+          print('begin sendCoin');
+          var seed = walletService.generateSeed(mnemonic);
+          print(seed);
+          var ret = await walletService.sendCoin(seed, 1, 'oV1KxZswBx2AUypQJRDEb2CsW2Dq2Wp4L5', 0.123);
+          print('ret==');
+          print(ret);
+
+
+
           setState(ViewState.Idle);
           return '';
         } else if (res.returnedText == 'Closed') {
