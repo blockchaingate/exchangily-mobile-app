5180ed6e6fe762638330c09240d3d92a4c6a9ece
barry-ruprai <barry.br@tutamail.com>
2020-09-25 10:29:55 -0400

    added scan barcode

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -294,7 +294,7 @@ class SettingsScreen extends StatelessWidget {
                   ),
                 ]),
           ),
-          bottomNavigationBar: BottomNavBar(count: 3),
+          bottomNavigationBar: BottomNavBar(count: 4),
         ),
       ),
     );
