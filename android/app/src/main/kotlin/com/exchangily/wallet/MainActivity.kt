2b91683aaa4f6a218f6182a6e3cf6ed229e3ee39
barry-ruprai <bhinderjit_ruprai@zoho.com>
2021-09-20 14:48:52 -0400

    fixed local auth bug(updated mainactivity file)

--- android/app/src/main/kotlin/com/exchangily/wallet/MainActivity.kt
+++ android/app/src/main/kotlin/com/exchangily/wallet/MainActivity.kt
@@ -1,9 +1,11 @@
+package com.exchangily.wallet;
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterFragmentActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
 }
