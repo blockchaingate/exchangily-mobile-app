6f44e32fd8559ee3920c1b944e97f761719d5d27
barry-ruprai <barry.br@tutamail.com>
2022-04-05 09:07:18 -0400

    2.1.49+205
    Moved add gas button and removed its swiper card and

--- lib/screens/wallet/wallet_dashboard_view.dart
+++ lib/screens/wallet/wallet_dashboard_view.dart
@@ -302,8 +302,6 @@ Widget topWidget(WalletDashboardViewModel model, BuildContext context) {
             //     ));
             // titleWidget = AppLocalizations.of(context).totalExchangeBalance;
             return TotalBalanceWidget2(model: model, index: index);
-          } else {
-            return TotalBalanceWidget3(model: model, index: index);
           }
 
           // return TotalBalanceCardWidget(
@@ -311,7 +309,7 @@ Widget topWidget(WalletDashboardViewModel model, BuildContext context) {
           //   title: titleWidget,
           // );
         },
-        itemCount: 3,
+        itemCount: 2,
         itemWidth: 500,
         itemHeight: 180.0,
         layout: SwiperLayout.TINDER,
@@ -457,6 +455,7 @@ Widget amountAndGas(WalletDashboardViewModel model, BuildContext context) {
                                 : primaryColor,
                           ),
                     Container(
+                      width: 100,
                       padding: const EdgeInsets.only(left: 5),
                       child: Text(
                         !model.isHideSmallAssetsButton
@@ -480,7 +479,9 @@ Widget amountAndGas(WalletDashboardViewModel model, BuildContext context) {
             ),
             //Add free FAB container
             !model.isFreeFabNotUsed
-                ? Container()
+                ? model.isBusy
+                    ? Container()
+                    : Expanded(child: Gas(gasAmount: model.gasAmount))
                 : Container(
                     margin: const EdgeInsets.symmetric(vertical: 5.0),
                     decoration: BoxDecoration(
