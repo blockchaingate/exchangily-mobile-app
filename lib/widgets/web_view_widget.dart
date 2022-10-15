31b0ea9adca368dc7b1f58e4b1ecba818e2fbe0c
barry-ruprai <b.code@tutamail.com>
2022-10-14 21:57:24 -0400

    v2.1.80+250
    updated web view widget

--- lib/widgets/web_view_widget.dart
+++ lib/widgets/web_view_widget.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
+import 'package:exchangilymobileapp/constants/colors.dart';
 import 'package:exchangilymobileapp/constants/custom_styles.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -13,13 +16,27 @@ class WebViewWidget extends StatefulWidget {
   State<WebViewWidget> createState() => _WebViewWidgetState(this.url);
 }
 
-class _WebViewWidgetState extends State<WebViewWidget> {
+class _WebViewWidgetState extends State<WebViewWidget>
+    with TickerProviderStateMixin {
+  bool isLoading = true;
+  int loadingProgress = 0;
+  AnimationController animationController;
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   void initState() {
     super.initState();
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 700), vsync: this);
+    animationController.repeat();
     if (Platform.isAndroid) {
       WebView.platform = SurfaceAndroidWebView();
     }
+    //if (Platform.isIOS) WebView.platform = CupertinoWebView();
   }
 
   final String _url;
@@ -27,23 +44,48 @@ class _WebViewWidgetState extends State<WebViewWidget> {
   _WebViewWidgetState(this._url);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        // appBar: AppBar(
-        //   centerTitle: true,
-        //   title: Text(
-        //     widget.title,
-        //     style: headText4,
-        //   ),
-        // ),
-        body: Column(
-      children: [
-        Expanded(
-            child: WebView(
-          key: _key,
-          javascriptMode: JavascriptMode.unrestricted,
-          initialUrl: _url,
-        ))
-      ],
-    ));
+    return SafeArea(
+      child: Scaffold(
+          body: Stack(
+        children: [
+          WebView(
+            // onProgress: ((progress) {
+            //   widget.onCallBack(progress);
+            //   setState(() {
+            //     loadingProgress = progress;
+            //   });
+            //   debugPrint('loadingProgress $loadingProgress');
+            // }),
+            onPageFinished: (finish) {
+              setState(() {
+                isLoading = false;
+              });
+            },
+            gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
+              Factory<VerticalDragGestureRecognizer>(
+                () => VerticalDragGestureRecognizer(),
+              ),
+            },
+            key: _key,
+            javascriptMode: JavascriptMode.unrestricted,
+            initialUrl: _url,
+            //initialUrl: Uri.dataFromString(
+            //   '<p>Web view sample</p>' * 1000,
+            //    ).toString(),
+          ),
+          Visibility(
+            child: Center(
+                child: CircularProgressIndicator(
+              color: primaryColor,
+              valueColor: animationController
+                  .drive(ColorTween(begin: secondaryColor, end: primaryColor)),
+              //  value: double.parse(loadingProgress.toString()) / 100,
+              semanticsValue: loadingProgress.toString(),
+            )),
+            visible: isLoading,
+          )
+        ],
+      )),
+    );
   }
 }
